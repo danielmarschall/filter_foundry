de710eadd71b1b43af58fb09397f0efb81d34142
Daniel Marschall <info@daniel-marschall.de>
2018-12-29 10:58:39 +0000

    (Small VC batch script fix)
    git-svn-id: https://svn.viathinksoft.com/svn/filter_foundry/trunk@125 1b5a0048-3058-4200-831f-25aa9e3a14b9

--- vc_make_3264.bat
+++ vc_make_3264.bat
@@ -1,12 +1,12 @@
 
-cd %~dp0
+cd "%~dp0"
 
-REM Adjust this path to your VC++ environment
+REM Please adjust this path to your VC++ environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
 nmake /f nmake.mak clean
 nmake /f nmake.mak
 
-REM Adjust this path to your VC++ environment
+REM Please adjust this path to your VC++ environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
 nmake /f nmake64.mak clean
 nmake /f nmake64.mak
