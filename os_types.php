b6453d3517351e5a29e23c11d46a68a385e68f5d
Daniel Marschall <info@daniel-marschall.de>
2021-11-01 00:36:33 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/filter_foundry/trunk@378 1b5a0048-3058-4200-831f-25aa9e3a14b9

--- os_types.php
+++ os_types.php
@@ -1,6 +1,28 @@
 #!/usr/bin/php
 <?php
 
+/*
+    This file is part of "Filter Foundry", a filter plugin for Adobe Photoshop
+    Copyright (C) 2003-2009 Toby Thain, toby@telegraphics.com.au
+    Copyright (C) 2018-2021 Daniel Marschall, ViaThinkSoft
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// This script generates the documentation file os_types.md
+
 define('DESC_WIDTH', 55);
 define('VENDOR_WIDTH', 15);
 
@@ -167,4 +189,3 @@ function pipl_property_header($title) {
 	echo "| C.code | Prpty. | Dec                   | Hex                   | ".str_pad('Description', DESC_WIDTH, " ", STR_PAD_RIGHT)." | ".str_pad('Vendor', VENDOR_WIDTH, " ", STR_PAD_RIGHT)." |\n";
 	echo "|--------|--------|-----------------------|-----------------------|".str_repeat('-', DESC_WIDTH+2)."|".str_repeat('-', VENDOR_WIDTH+2)."|\n";
 }
-
