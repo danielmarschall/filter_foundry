84c252be7813bcb42b3f2059f043bd7962dfa679
Daniel Marschall <info@daniel-marschall.de>
2021-11-27 23:25:50 +0000

    Fixed severe crash when sliders were created and moved, then the filter was applied and re-opened!!!
    git-svn-id: https://svn.viathinksoft.com/svn/filter_foundry/trunk@421 1b5a0048-3058-4200-831f-25aa9e3a14b9

--- read.c
+++ read.c
@@ -62,8 +62,8 @@ Boolean readparams_afs_pff(Handle h,char **reason){
 
 		if(c==CR || c==LF){ /* detected end of line */
 
-			/* look ahead to see if we need to skip a line feed (DOS EOL convention) */
-			if(c == CR && c == LF && p < dataend)
+			/* look ahead to see if we need to skip a line feed (DOS CRLF EOL convention) */
+			if(p < dataend && c == CR && *p == LF)
 				++p;
 
 			linebuf[lineptr] = 0; /* add terminating NUL to line buffer */
